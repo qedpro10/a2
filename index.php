aa94a9a6cd6e55518827314a59b5ea155456b7bf
Jens Smith <qedpro@hotmail.com>
2017-02-10 18:03:57 -0800

    Initial dev files setup

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php require('getInfo.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+   <h1>Assignment #2. Your obsessions are exhausting</h1>
+</body>
+
+</html>
